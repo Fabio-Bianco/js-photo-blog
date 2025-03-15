fd2c07dea39bff2dd0d26ab0f852de666bb81827
Fabio-Bianco <biancobfabio@gmail.com>
2025-03-16 00:30:37 +0100

    feat: call AJAX API javascript

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 
+// script.js
+
+const photoContainer = document.getElementById("photo-container");
+
+axios.get("https://lanciweb.github.io/demo/api/pictures/")
+    .then(response => {
+        response.data.forEach(item => {
+            const card = document.createElement("div");
+            card.classList.add("photo-card");
+
+            card.innerHTML = `
+                <img src="img/pin.svg" class="pin" alt="Pin">
+                <div class="photo">
+                    <img src="${item.image}" alt="">
+                </div>
+                <p class="caption">${item.caption}</p>
+            `;
+
+            photoContainer.appendChild(card);
+        });
+    })
+    .catch(error => console.error("Errore nel caricamento delle immagini:", error));
