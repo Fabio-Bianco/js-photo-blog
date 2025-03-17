ce3046a6522723857d5f4508b57fd1d0e8d2f97c
Fabio-Bianco <biancobfabio@gmail.com>
2025-03-17 12:35:17 +0100

    feat: completed ex pt 1

--- script.js
+++ script.js
@@ -42,19 +42,19 @@ const photoContainer = document.getElementById("photo-container");
 axios
   .get("https://lanciweb.github.io/demo/api/pictures/")
   .then((response) => {
-    // Limitiamo a 5 nuove card (la prima è già presente)
     response.data.slice(0, 5).forEach((obj) => {
       const card = document.createElement("div");
       card.classList.add("photo-card");
 
       card.innerHTML = `
-                <img src="img/pin.svg" class="pin" alt="Pin">
-                <div class="photo">
-                    <img src="${obj.image}" alt="">
-                </div>
-                <p class="caption">${obj.caption}</p>
-            `;
-
+        <img src="img/pin.svg" class="pin" alt="Pin">
+        <div class="photo">
+          <img class="img-in" src="${obj.url}" alt="${obj.url}">
+        </div>
+        <p class="caption">${obj.date}</p>
+        <p class="title">${obj.title}</p>
+        
+      `;
       photoContainer.appendChild(card);
     });
   })
